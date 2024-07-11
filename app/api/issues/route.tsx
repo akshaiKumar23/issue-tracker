7cf6b0f322347951fd3af2c5120d4575863c1048
Johan-Monster <johanliebert1205@gmail.com>
2024-07-11 18:26:14 +0530

    Client Side error handling

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -2,11 +2,8 @@ import prisma from "@/prisma/client";
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import { createIssueSchema } from "@/app/validationSchema";
 
-const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required").max(255),
-  description: z.string().min(3, "Description is required"),
-});
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);

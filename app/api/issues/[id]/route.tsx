ba3f6122402d20038e92ec1434da418ceca7b4d3
Johan-Monster <johanliebert1205@gmail.com>
2024-07-12 16:27:18 +0530

    Build the edit issue page and api to update the issue

---
+++ app/api/issues/[id]/route.tsx
@@ -0,0 +1,40 @@
+import { createIssueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: {
+      id: issue.id,
+    },
+    data: {
+      title: issue.title,
+      description: issue.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}

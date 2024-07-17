ad64c0085fb156ef01c7240f3b14c9385034e735
Johan-Monster <johanliebert1205@gmail.com>
2024-07-17 16:20:39 +0530

    commit

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -7,7 +7,7 @@ import { IssueStatusBadge } from "./components";
 const LatestIssues = async () => {
   const issues = await prisma.issue.findMany({
     orderBy: {
-      createdAt: "desc",
+      createdAt: "asc",
     },
     take: 5,
     include: {

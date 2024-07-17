5c4222967498338495568f25e4f91fb18c189cf0
Johan-Monster <johanliebert1205@gmail.com>
2024-07-17 12:03:55 +0530

    add the meta data

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -41,3 +41,13 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
   );
 };
 export default IssueDetailPage;
+
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  return {
+    title: issue?.title,
+    description: "Details of issue" + issue?.id,
+  };
+}
